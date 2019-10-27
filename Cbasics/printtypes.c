45d8efd72f5f878f2588017f6337a65bc84b3a08
NaNdalal-dev <dnandalal7@gmail.com>
2019-10-27 20:38:12 +0530

    dtypes

---
+++ Cbasics/printtypes.c
@@ -0,0 +1,11 @@
+//printing types
+#include<stdio.h>
+int main()
+{
+
+    printf("Int :%d",32);
+    printf("\nFloat :%f",3.456);
+    printf("\nDouble:%e",3.e34);
+    printf("\nCharacter:%c",'c');
+    return 0;
+}
