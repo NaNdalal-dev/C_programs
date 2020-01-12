9c39e1288c051afa32e2dbb6b3f5b2d0ec0f8541
NaNdalal-dev <56131112+NaNdalal-dev@users.noreply.github.com>
2020-01-12 16:10:46 +0530

    Return

--- Arrays/sorting/insertion_sort.c
+++ Arrays/sorting/insertion_sort.c
@@ -40,5 +40,39 @@ for(i=0;i<size;++i)
 {
 printf("%d ",a[i]);
 }
+return 0;//Program for linear search
+#include<stdio.h>
+int main()
+{
+int a[10],i,srch,flag=0;
 
+//Initialisiing searching number
+printf("Enter 10 elements: \n");
+for(i=0;i<10;++i)
+{
+printf("Enter a[%d] value :",i);
+scanf("%d",&a[i]);
+}
+//initialising searching number
+printf("Enter searching number : ");
+scanf("%d",&srch);
+
+//searching operation
+for(i=0;i<10;++i)
+{
+if(srch==a[i])
+{
+printf("%d is found at a[%d]",srch,i);
+flag=1;
+break;
+}
+}
+
+if(flag==0)
+{
+printf("%d is not found \nsearch failed!",srch);
+}
+
+}
+return 0;
 }
